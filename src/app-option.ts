e56835442a6a4d007667130bdf3fbd0a6b56913e
Jim Campbell <jcdevguru@gmail.com>
2024-11-01 18:38:21 -0400

    Simplified appOptionToken definition to avoid enum

--- src/app-option.ts
+++ src/app-option.ts
@@ -2,9 +2,6 @@ import { CompareAppOption, isCompareAppOption } from './compare-option';
 import { RenderAppOption, isRenderAppOption } from './render-option';
 
 import {
-  isEnumMember,
-  isStandardObject,
-  anyToString,
   verifyObject,
   type AtLeastOne,
 } from './util';
@@ -16,50 +13,36 @@ export interface AppOptionObject {
   render: RenderAppOption
 }
 
-export enum AppOptionToken {
-  Exact = 'Exact',
-  General = 'General',
-}
+// Predefined tokens
+const APP_OPTION_TOKENS = ['Exact', 'General'] as const;
+export type AppOptionToken = typeof APP_OPTION_TOKENS[number];
+export const isAppOptionToken = (v: unknown): v is AppOptionToken => 
+  APP_OPTION_TOKENS.includes(v as AppOptionToken);
 
 export type MinimalAppOptionObject = AtLeastOne<AppOptionObject>;
-
-export type AppOptions = AppOptionToken | keyof typeof AppOptionToken | MinimalAppOptionObject;
-
-export const isAppOptionToken = (v: unknown): v is AppOptionToken => isEnumMember(v, AppOptionToken);
 
 export const isMinimalAppOptionObject = (v: unknown, errs?: Array<string>): v is MinimalAppOptionObject => verifyObject(v, {
   compare: isCompareAppOption,
   render: isRenderAppOption,
 }, true, errs);
 
+export type AppOptions = AppOptionToken | MinimalAppOptionObject;
 export const isAppOptions = (v: unknown): v is AppOptions => isAppOptionToken(v) || isMinimalAppOptionObject(v);
 
 export const validateAppOptions = (v: unknown): v is CompareAppOption => {
-  const aType = typeof v;
   const errors: Array<string> = [];
 
-  switch (aType) {
-    case 'string':
-      if (!isAppOptionToken(v)) {
-        throw new OptionError('invalid token for application option', v as string);
-      }
-      break;
-
-    case 'object':
-      if (!isStandardObject(v)) {
-        throw new OptionError('invalid object type for app option', anyToString(v));
-      }
-      if (!isMinimalAppOptionObject(v, errors)) {
-        let s = 'app option object is not valid';
-        if (errors.length) {
-          s = `${s}: errors: ${errors.join(', ')}`;
-        }
-        throw new OptionError(s);
-      }
-      break;
-
-    default:
-      throw new OptionError(`invalid app option type ${aType}`);
+  if (isAppOptionToken(v)) {
+    return true;
   }
+
+  if (!isMinimalAppOptionObject(v, errors)) {
+    let s = 'app option object is not valid';
+    if (errors.length) {
+      s = `${s}: errors: ${errors.join(', ')}`;
+    }
+    throw new OptionError(s);
+  }
+
   return true;
 };
