d1b9612dcc9a8554f780ac1b26b568ba9ef19855
Jim Campbell <jcdevguru@gmail.com>
2024-12-14 20:14:24 -0500

    Beginning package configurations.

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,8 @@ import type { Value } from './lib/types';
 
 // do nothing for now
 export default (v1: Value, v2: Value): Value => ({ left: v1, right: v2 });
+
+export { default as CoreCompare } from './compare';
+// Only export what you want public
+export type { CompareResult, CompareFunction } from './compare/types';
+export type { Option } from './lib/option';
