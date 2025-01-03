7c184517cb0cfd4180d8557f0be775cede4bf799
Jim Campbell <jcdevguru@gmail.com>
2025-01-03 09:34:18 -0500

    matching set test

--- src/__tests__/102simple-composite.test.ts
+++ src/__tests__/102simple-composite.test.ts
@@ -18,5 +18,16 @@ describe('successful compare', () => {
     });
   };
 
+  const testMatchingSet = (testName: string, left: Value, right: Value, options?: MinimalConfigOptions) => {
+    test(compareTestLabel(testName, options), () => {
+      const c = new Compare(options);
+      expect(c.compare(left, right).result).toStrictEqual({
+        leftSame: [expect.objectContaining({value: left})],
+        rightSame: [expect.objectContaining({value: right})]
+      });
+    });
+  };
+
   testMismatchingSet('mismatching sets', new Set([1, 2, 3]), new Set([1, 2, 4]), { compare: { compareSet: 'valuesOnly' } });
+  testMatchingSet('matching sets', new Set([1, 2, 3]), new Set([1, 2, 3]), { compare: { compareSet: 'valuesOnly' } });
 });
