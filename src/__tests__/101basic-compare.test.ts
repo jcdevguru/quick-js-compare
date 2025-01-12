6120b00feff3f693132423c3ce9c4c282b609008
Jim Campbell <jcdevguru@gmail.com>
2025-01-12 14:56:40 -0500

    No longer using 'testMatch' and 'testMismatch' for maintainability sake.  Now can skip or only individual tests.

--- src/__tests__/101basic-compare.test.ts
+++ src/__tests__/101basic-compare.test.ts
@@ -1,38 +1,56 @@
 // These tests are for basic operations of the compare function
 
 import { type MinimalConfigOptions } from '@lib/option';
-import { type Value } from '@lib/types';
 import { verifyCompare, compareTestLabel } from '@test/util';
 
 describe('compare - basic operations', () => {
-  const testMatch = (testName: string, left: Value, right: Value, options: MinimalConfigOptions) => {
-    test(compareTestLabel(testName, options), () => {
-      verifyCompare(left, right, options, true);
-    });
-  };
-  
-  const testMismatch = (testName: string, left: Value, right: Value, options: MinimalConfigOptions) => {
-    test(compareTestLabel(testName, options), () => {
-      verifyCompare(left, right, options, false);
-    });
-  };
-
   const exactOptions: MinimalConfigOptions = { compare: 'Exact' };
 
-  testMatch('exact matching strings', 'test-string1', 'test-string1', exactOptions);
-  testMatch('exact matching numbers', 1, 1, exactOptions);
-  testMatch('exact matching booleans', true, true, exactOptions);
+  test(compareTestLabel('exact matching strings', exactOptions), () => {
+    verifyCompare('test-string1', 'test-string1', exactOptions, true);
+  });
 
-  testMismatch('mismatching strings', 'test-string1', 'test-string2', exactOptions);
-  testMismatch('mismatching numbers', 1, 2, exactOptions);
-  testMismatch('mismatching booleans', true, false, exactOptions);
+  test(compareTestLabel('exact matching numbers', exactOptions), () => {
+    verifyCompare(1, 1, exactOptions, true);
+  });
 
-  testMatch('abstract matching scalars 1', 0, false, { compare: 'General' });
-  testMatch('abstract matching scalars 2', '', 0, { compare: 'General' });
+  test(compareTestLabel('exact matching booleans', exactOptions), () => {
+    verifyCompare(true, true, exactOptions, true);
+  });
 
-  testMatch('scalar type only', 1, 91, { compare: { compareScalar: 'typeOnly' } });
-  testMatch('array type only', [1, 2, 3], ['abc'], { compare: { compareArray: 'typeOnly' } });
+  test(compareTestLabel('mismatching strings', exactOptions), () => {
+    verifyCompare('test-string1', 'test-string2', exactOptions, false);
+  });
 
-  testMismatch('scalars and composites - always mismatch', 'mismatch-me', ['mismatch-me'], exactOptions);
-  testMismatch('mismatching scalar types', 1, '1', exactOptions);
+  test(compareTestLabel('mismatching numbers', exactOptions), () => {
+    verifyCompare(1, 2, exactOptions, false);
+  });
+
+  test(compareTestLabel('mismatching booleans', exactOptions), () => {
+    verifyCompare(true, false, exactOptions, false);
+  });
+
+  test(compareTestLabel('abstract matching scalars 1', { compare: 'General' }), () => {
+    verifyCompare(0, false, { compare: 'General' }, true);
+  });
+
+  test(compareTestLabel('abstract matching scalars 2', { compare: 'General' }), () => {
+    verifyCompare('', 0, { compare: 'General' }, true);
+  });
+
+  test(compareTestLabel('scalar type only', { compare: { compareScalar: 'typeOnly' } }), () => {
+    verifyCompare(1, 91, { compare: { compareScalar: 'typeOnly' } }, true);
+  });
+
+  test(compareTestLabel('array type only', { compare: { compareArray: 'typeOnly' } }), () => {
+    verifyCompare([1, 2, 3], ['abc'], { compare: { compareArray: 'typeOnly' } }, true);
+  });
+
+  test(compareTestLabel('scalars and composites - always mismatch', exactOptions), () => {
+    verifyCompare('mismatch-me', ['mismatch-me'], exactOptions, false);
+  });
+
+  test(compareTestLabel('mismatching scalar types', exactOptions), () => {
+    verifyCompare(1, '1', exactOptions, false);
+  });
 });
